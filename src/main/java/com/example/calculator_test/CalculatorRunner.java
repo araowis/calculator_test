f56642bf35718127ad5c94dd920af20aff9d9570
araowis <aditi.rao@wissen.com>
2026-02-25 17:03:52 +0530

    reduced to 10

--- src/main/java/com/example/calculator_test/CalculatorRunner.java
+++ src/main/java/com/example/calculator_test/CalculatorRunner.java
@@ -18,8 +18,8 @@ public class CalculatorRunner implements CommandLineRunner {
 
         while (true) { 
             try {
-                int a = (int) (Math.random() * 1000); 
-                int b = (int) (Math.random() * 1000); 
+                int a = (int) (Math.random() * 10); 
+                int b = (int) (Math.random() * 10); 
                 System.out.println("Adding " + a + " + " + b + " = " + calculatorService.add(a, b));
                 System.out.println("Subtracting " + a + " - " + b + " = " + calculatorService.subt(a, b));
                 System.out.println("Multiplying " + a + " * " + b + " = " + calculatorService.mul(a, b));
